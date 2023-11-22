aba46405653bf4003ac9b4617e8551b08cf6b7b1
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-11-22 12:38:41 +0100

    image and rating can be null

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -8,11 +8,27 @@ export const ProductId = Sc.number.pipe(Sc.brand('ProductId'))
 const OptionalProductId = Sc.transformOrFail(
   Sc.union(Sc.string, Sc.number),
   Sc.union(ProductId, Sc.undefined),
-  id => typeof id === 'string' && id.length === 0 ? PR.success(undefined) : PR.success(+id),
+  id =>
+    typeof id === 'string' && id.length === 0 && !Number.isNaN(id) ?
+      PR.success(undefined) :
+      PR.success(+id),
   id =>
     id === undefined ?
       PR.fail(PR.parseError([PR.type(ProductId.ast, id, 'Id is undefined')])) :
       PR.success(id)
+)
+
+const NumberFromString = Sc.transformOrFail(
+  Sc.union(Sc.string, Sc.number),
+  Sc.number,
+  number =>
+    typeof number === 'string' && number.length === 0 && !Number.isNaN(number) ?
+      PR.success(0) :
+      PR.success(+number),
+  number =>
+    number === undefined ?
+      PR.fail(PR.parseError([PR.type(ProductId.ast, number, 'Number is undefined')])) :
+      PR.success(number)
 )
 
 export const Product = Sc.struct({
@@ -20,12 +36,12 @@ export const Product = Sc.struct({
   code: Sc.string,
   name: Sc.string,
   description: Sc.string,
-  price: Sc.number,
-  quantity: Sc.number,
+  price: NumberFromString,
+  quantity: NumberFromString,
   inventoryStatus: Sc.literal('INSTOCK', 'LOWSTOCK', 'OUTOFSTOCK'),
   category: Sc.literal('Accessories', 'Clothing', 'Electronics', 'Fitness'),
-  image: Sc.optional(Sc.string),
-  rating: Sc.optional(Sc.number)
+  image: Sc.optional(Sc.union(Sc.string, Sc.null)),
+  rating: Sc.optional(Sc.union(Sc.number, Sc.null))
 })
 
 export interface Product extends Sc.Schema.To<typeof Product> {}
